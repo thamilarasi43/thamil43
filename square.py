85669c0d43fb25bb30b099680288364375cbef8b
thamilarasi43 <37330896+thamilarasi43@users.noreply.github.com>
2018-05-19 14:05:28 +0530

    Create square.py

---
+++ square.py
@@ -0,0 +1,12 @@
+ax=int(input("enter a-x"))
+ay=int(input("enter a-y"))
+bx=int(input("enter b-x"))
+by=int(input("enter b-y"))
+cx=int(input("enter c-x"))
+cy=int(input("enter c-y"))
+dx=int(input("enter d-x"))
+dy=int(input("enter d-y"))
+if ax==ay and ay==bx and by==cx and cx==cy and cy==dx and dy==ax:
+    print("yes")
+else:
+    print("no")
