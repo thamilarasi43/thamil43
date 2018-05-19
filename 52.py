27ecd890063c384e36b4fb134b943992b4c7db1c
thamilarasi43 <37330896+thamilarasi43@users.noreply.github.com>
2018-05-19 14:09:17 +0530

    Create 52.py

---
+++ 52.py
@@ -0,0 +1,12 @@
+ax1=int(input("enter a-x"))
+ay1=int(input("enter a-y"))
+bx1=int(input("enter b-x"))
+by1=int(input("enter b-y"))
+cx1=int(input("enter c-x"))
+cy1=int(input("enter c-y"))
+dx1=int(input("enter d-x"))
+dy1=int(input("enter d-y"))
+if ax==ay and ay==bx and by==cx and cx==cy and cy==dx and dy==ax:
+    print("yes")
+else:
+    print("no")
